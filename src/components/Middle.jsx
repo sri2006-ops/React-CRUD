7f355f0c3ea28b91a9fa2a3970b72de2ab880b5f
sri2006-ops <jayanthasrim@gmail.com>
2026-05-08 10:43:15 +0530

    modified middle file

--- src/components/Middle.jsx
+++ src/components/Middle.jsx
@@ -1,15 +1,41 @@
 function Middle() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    alert("Form Submitted!");
+  };
+
   return (
     <div className="middle">
-      <h1>Welcome!!!</h1>
+      <h1>Registration Form</h1>
 
-      
+      <form onSubmit={handleSubmit} className="form">
 
-      <input
-        type="text"
-        placeholder="Search here..."
-        className="search-bar"
-      />
+        <input
+          type="text"
+          placeholder="Enter Name"
+        />
+
+        <input
+          type="email"
+          placeholder="Enter Email"
+        />
+
+        <input
+          type="number"
+          placeholder="Enter Age"
+        />
+
+        <input
+          type="tel"
+          placeholder="Enter Mobile Number"
+        />
+
+        <button type="submit">
+          Submit
+        </button>
+
+      </form>
     </div>
   );
 }
